f735a667371c0c13542ce30118ecf660bafd13e5
Farrizal Alchudry Mutaqien <farrizalchudry@gmail.com>
2019-05-21 09:26:06 +0700

    Add API Documentation

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 const fastify = require('fastify')({
   logger: true
 })
+const fastswagger = require('fastify-swagger')
 const mongoose = require('mongoose')
+const swagger = require('./config/swagger')
+
+// Register Documentation with Swagger
+fastify.register(fastswagger, swagger.options)
 
 // DB Constanta
 const dbUrl = 'mongodb://localhost/sorabel'
 const dbOption =  {
   useNewUrlParser: true
 }
+
 // Connect to DB
 mongoose.connect(dbUrl, dbOption).then(() => {
   console.log('MongoDB connected…')
@@ -17,7 +23,7 @@ mongoose.connect(dbUrl, dbOption).then(() => {
 
 // Import Route
 const routes = require('./routes')
-
+// Add all available route to App
 routes.forEach((route, index) => {
   fastify.route(route)
 })
